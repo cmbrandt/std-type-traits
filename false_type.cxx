288a589f68f93dd0ad9cdc7bedaafd5fd1eede8b
Christopher Brandt <cmb199@zips.uakron.edu>
2021-01-11 16:51:08 -0600

    Adding example programs for helper types to directory.

---
+++ false_type.cxx
@@ -0,0 +1,10 @@
+// bool_constant.cxx
+
+#include <iostream>
+
+
+int main()
+{
+
+  std::cout << "\nDone." << std::endl;
+}
--- include/type_traits.hxx
+++ include/type_traits.hxx
@@ -15,6 +15,7 @@ template <class T, T v> struct integral_constant;
 
 template <bool B>
   using bool_constant = integral_constant<bool, B>;
+
 using true_type  = bool_constant<true>;
 using false_type = bool_constant<false>;
 
