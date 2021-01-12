ab1ddd329a7245649796f56f508f9b9ba561fd6c
Christopher Brandt <cmb199@zips.uakron.edu>
2021-01-12 17:14:34 -0600

    Working on is_same and conditional implementations.

--- include/type_traits.hxx
+++ include/type_traits.hxx
@@ -14,12 +14,11 @@ namespace cmb {
 template <class T, T v> struct integral_constant;
 
 template <bool B>
-  using bool_constant = integral_constant<bool, B>;
+using bool_constant = integral_constant<bool, B>;
 
 using true_type  = bool_constant<true>;
 using false_type = bool_constant<false>;
 
-///*
 template <class T> struct is_void;
 template <class T> struct is_null_pointer;
 template <class T> struct is_integral;
@@ -32,27 +31,15 @@ template <class T> struct is_union;
 template <class T> struct is_class;
 template <class T> struct is_function;
 
-template <class T> struct is_same;
+template <class T, class U> struct is_same;
 
 template <class T> struct remove_const;
 template <class T> struct remove_volatile;
 template <class T> struct remove_cv;
 
+template <bool B, class T, class F> struct conditional;
+
 /*
-tempalte <class T>
-  using remove_const_t    = typename remove_const<T>::type;
-tempalte <class T>
-  using remove_volatile_t = typename remove_volatile_t<T>::type;
-tempalte <class T>
-  using remove_cv_t       = typename remove_cv<T>::type;
-
-template <class T, class F> struct conditional;
-
-template <bool b, class T, class F>
-  using conditional_t = typename conditional<b, T, F>::type;
-
-template <class...>
-  using void_t = void;
 
 template <class T>
   using inline constexpr bool is_void_v           = is_void<T>::value;
@@ -80,32 +67,39 @@ template <class T>
 
 
 //
-// Helper classes
+// Helper class
 
 template <class T, T v>
 struct integral_constant {
 
-    static constexpr T value = v;
+  static constexpr T value = v;
 
-    using value_type = T;
-    using type       = integral_constant; // using injected-class-name
+  using value_type = T;
+  using type       = integral_constant; // using injected-class-name
 
-    constexpr operator   value_type() const noexcept { return value; }
-    constexpr value_type operator()() const noexcept { return value; }
+  constexpr operator   value_type() const noexcept { return value; }
+  constexpr value_type operator()() const noexcept { return value; }
 };
 
 
-
+//
 // Primary type categories
 
 
 
-// Composite type categories
 
+//
+// Type relationships
+
+template <class T, class U> struct is_same      : cmb::false_type { };
+template <class T>          struct is_same<T,T> : cmb::true_type  { };
+
+template <class T, class U>
+  constexpr bool is_same_v = is_same<T,U>::value;
 
 
 //
-// Const-volatily specifiers
+// Const-volatile specifiers
 
 template <class T> struct remove_const<const T>       { using type = T; };
 template <class T> struct remove_volatile<volatile T> { using type = T; };
@@ -119,6 +113,20 @@ template <class T>
   using remove_volatile_t = typename remove_volatile<T>::type;
 template <class T>
   using remove_cv_t       = typename remove_cv<T>::type;
+
+
+//
+// Other transformations
+
+template <bool B, class T, class F>
+struct conditional { using type = T; };
+
+template <class T, class F>
+struct conditional<false, T, F> { using F = type; };
+
+template <bool B, class T, class F>
+  using conditional_t = typename conditional<B,T,F>::type;
+
 
 } // namespace cmb
 
