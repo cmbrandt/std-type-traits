6767a3505d5010d5322c0d97c605cfb1b0793bbf
Christopher Brandt <cmb199@zips.uakron.edu>
2021-01-11 19:24:36 -0600

    Added const and volatility specifiers to header file.  Will test and complete examples tomorrow.

--- include/type_traits.hxx
+++ include/type_traits.hxx
@@ -96,17 +96,29 @@ struct integral_constant {
 
 
 
-// ...
+// Primary type categories
 
 
 
-// ...
+// Composite type categories
 
 
 
 //
-// ...
+// Const-volatily specifiers
 
+template <class T> struct remove_const<const T>       { using type = T; };
+template <class T> struct remove_volatile<volatile T> { using type = T; };
+template <class T> struct remove_cv<const T>          { using type = T; };
+template <class T> struct remove_cv<volatile T>       { using type = T; };
+template <class T> struct remove_cv<const volatile T> { using type = T; };
+
+template <class T>
+  using remove_const_t    = typename remove_const<T>::type;
+template <class T>
+  using remove_volatile_t = typename remove_volatile<T>::type;
+template <class T>
+  using remove_cv_t       = typename remove_cv<T>::type;
 
 } // namespace cmb
 
