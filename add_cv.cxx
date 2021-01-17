d121aa156e2c361be18b7fd42e7c6c47ffe7f746
Chris Brandt <cmb199@zips.uakron.edu>
2021-01-16 21:26:11 -0600

    add_cv example and impl complete.

--- add_cv.cxx
+++ add_cv.cxx
@@ -9,9 +9,9 @@ int main()
   using type1 = cmb::add_cv<int>::type;
   using type2 = cmb::add_cv_t<int>;
 
-  std::cout <<   "cmb::is_same_v<int, type1> = "
-            <<    cmb::is_same_v<int, type1> // = 1
-            << "\ncmb::is_same_v<int, type2> = "
-            <<    cmb::is_same_v<int, type2> // = 1
+  std::cout <<   "cmb::is_same_v<int const volatile, type1> = "
+            <<    cmb::is_same_v<int const volatile, type1> // = 1
+            << "\ncmb::is_same_v<int const volatile, type2> = "
+            <<    cmb::is_same_v<int const volatile, type2> // = 1
             << std::endl;
 }
